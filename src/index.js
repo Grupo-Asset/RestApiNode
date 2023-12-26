367313c09989eddcf2e81ae1d5b295bdd17e6811
franGhisoni <ghisonifran@gmail.com>
2023-12-26 11:18:53 -0300

    CORS

--- src/index.js
+++ src/index.js
@@ -34,8 +34,14 @@ app.use(cors({
     origin: (origin, callback) => {
         const ACCEPTED_ORIGINS = [
             'http://localhost:3000',
-            
+            'https://grupo-asset.com'
         ]
+        if (ACCEPTED_ORIGINS.includes(origin)){
+            return callback(null,true)
+        }if(!origin){
+            return callback(null,true)
+        }
+        return callback(new Error('Not allowed by CORS'))
     }}));
 
 // Routes
