654a476f4effbe2c92b2e09363a6b29d0a05e799
franGhisoni <ghisonifran@gmail.com>
2024-04-12 15:39:48 -0300

    cors all origins

--- src/index.js
+++ src/index.js
@@ -24,21 +24,22 @@ app.get("/", function (req, res) {
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({
-    origin: (origin, callback) => {
-        const ACCEPTED_ORIGINS = [
-            'http://localhost:3000',
-            'https://grupo-asset.com'
-        ]
-        if (ACCEPTED_ORIGINS.includes(origin)){
-            return callback(null,true)
-        }if(!origin){
-            return callback(null,true)
-        }
-        return callback(new Error('Not allowed by CORS'))
-    }}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         const ACCEPTED_ORIGINS = [
+//             'http://localhost:3000',
+//             'https://grupo-asset.com',
 
+//         ]
+//         if (ACCEPTED_ORIGINS.includes(origin)){
+//             return callback(null,true)
+//         }if(!origin){
+//             return callback(null,true)
+//         }
+//         return callback(new Error('Not allowed by CORS'))
+//     }}));
 
+app.use(cors());
 import getFacturaPDFRouter from './routes/getFacturaPDF.js';
 
 import { inventoryRouter } from './routes/inventory.js';
