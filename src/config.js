7009f94f97d0bafe5fff1cb99048cbd891d21f58
franGhisoni <ghisonifran@gmail.com>
2024-04-12 12:29:42 -0300

    host env

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import {config} from "dotenv";
 config();
 
 export const PORT = 8080
-export const HOST = "http://localhost:8080"
+export const HOST = process.env.HOST;
 // export const HOST = "https://prod-testing-development.up.railway.app"
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
